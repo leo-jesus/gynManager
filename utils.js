2dd58b7744d5688b5eb5c96b1d1ff3bb0356b633
leo-jesus <llxdgb1@gmail.com>
2020-07-22 00:02:48 -0300

    limpeza do código

--- utils.js
+++ utils.js
@@ -19,15 +19,9 @@ module.exports = {
     },
     date: function (timestamp) {
         const date = new Date(timestamp)
-        //yyyy
         const year = date.getUTCFullYear()
-        //mm
         const month = `0${date.getUTCMonth() + 1}`.slice(-2)
-        //dd
         const day = `0${date.getUTCDate()}`.slice(-2)
-
-        //return yyyy-mm-dd
-
        return`${year}-${month}-${day}`
     }
 }
